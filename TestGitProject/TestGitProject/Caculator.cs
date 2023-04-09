4c303dff36acd461cf675cf7b1e7b5e422708e67
Farhad Zamiri <farhad.zamiri@gmail.com>
2023-04-09 14:57:57 +0200

    Add all other operations

--- TestGitProject/TestGitProject/Caculator.cs
+++ TestGitProject/TestGitProject/Caculator.cs
@@ -12,5 +12,20 @@ namespace TestGitProject
         {
             return a + b;
         }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Divide(int a, int b)
+        {
+            return a / b;
+        }
     }
 }
